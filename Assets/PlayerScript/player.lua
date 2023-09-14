d3dd6eb5cea8b3c4a87dfc29adcbce39b502f9d5
柠凉 <1292571653@qq.com>
2023-09-14 12:06:28 +0800

    添加部分注释

--- Assets/PlayerScript/player.lua
+++ Assets/PlayerScript/player.lua
@@ -1,4 +1,4 @@
-onGrund = false
+onGrund = false     --是否在地面上
 
 function Awake()
 
@@ -10,15 +10,17 @@ end
 
 function Update()
 
-    v = self:GetVelocity()
-    input = self:GetInput()
+    v = self:GetVelocity()      -- 获取角色当前的速度
+    input = self:GetInput()     -- 获取玩家的输入
 
-    if Input.GetKeyDown(KeyCode.Space) and onGrund then
+    
+    if Input.GetKeyDown(KeyCode.Space) and onGrund then 
+        -- 如果玩家按下空格键并且角色在地面上
         onGrund = false
-        v.y = 12
+        v.y = 12                -- 将角色的垂直速度设置为12，跳跃
     end
 
-    self:SetVelocity(input.x * 5, v.y)
+    self:SetVelocity(input.x * 5, v.y)  -- 根据玩家输入设置角色的水平速度
 end
 
 function FixedUpdate()
--- Assets/Script/Manager/CameraContorller.cs
+++ Assets/Script/Manager/CameraContorller.cs
@@ -6,9 +6,9 @@ using UnityEngine.EventSystems;
 public class CameraContorller : MonoBehaviour
 {
     [SerializeField]
-    private Camera view_cam;
+    private Camera view_cam;        //相机对象
 
-    public float s = 0.001f;
+    public float s = 0.001f;        // 鼠标移动速度的缩放因子
 
 
     // Start is called before the first frame update
@@ -17,15 +17,17 @@ public class CameraContorller : MonoBehaviour
         
     }
 
-    Vector3 last = Vector3.zero;
-    float mouseSpeed = 10;
+    Vector3 last = Vector3.zero;    // 上一帧鼠标位置
+    float mouseSpeed = 10;          // 鼠标移动速度
     // Update is called once per frame
     void Update()
     {
+        // 使用滚轮控制相机的缩放
         view_cam.orthographicSize = Mathf.Clamp(view_cam.orthographicSize + Input.mouseScrollDelta.y, 3, 7);
 
         if(Input.GetMouseButtonDown(1)) last = Input.mousePosition;
 
+        // 当按住鼠标右键时，根据鼠标移动的距离来移动相机位置
         if (Input.GetMouseButton(1))
         {
             Vector3 deltaMousePosition = Input.mousePosition - last;
@@ -35,6 +37,6 @@ public class CameraContorller : MonoBehaviour
             view_cam.transform.position += deltaMousePosition;
         }
 
-        last = Input.mousePosition;
+        last = Input.mousePosition; // 更新鼠标位置
     }
 }
